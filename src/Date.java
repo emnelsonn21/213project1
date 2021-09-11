53c8eb50e14ade111d5adf02ef9857c45ff163db
emnelsonn21 <55959987+emnelsonn21@users.noreply.github.com>
2021-09-11 17:47:17 -0400

    Update Date.java

--- src/Date.java
+++ src/Date.java
@@ -1,11 +1,28 @@
+import java.util.Calendar
 
 public class Date implements Comparable<Date> {
 	private int year;
 	private int month;
 	private int day;
 	
-	public Date(String date) {}
-	public Date() {}
+	//absolutely no idea if this is even remotely correct
+	public Date(String date) {
+		StringTokenizer tokens = new StringTokenizer(date, "/")
+		day = tokens.nextToken;
+		month = tokens.nextToken;
+		year = tokens.nextToken;
+		
+		Calendar calendar = Calendar.getInstance();
+		calendar.clear;
+		calendar.set(Calendar.YEAR, year);
+		calendar.set(Calendar.MONTH, month);
+		calendar.set(Calendar.DATE, day);
+	
+	}
+	
+	public Date() {
+		Calendar calendar = Calendar.getInstance();
+	}
 	
 	public boolean isValid() {}
 	

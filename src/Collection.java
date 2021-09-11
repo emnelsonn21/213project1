b63778b1a7645766cfbefe812ffb2c8ae2ce8ab0
emnelsonn21 <55959987+emnelsonn21@users.noreply.github.com>
2021-09-11 16:09:58 -0400

    Update Collection.java

--- src/Collection.java
+++ src/Collection.java
@@ -3,7 +3,21 @@ public class Collection {
 	private Album[] albums;
 	private int numAlbums;
 	private int find(Album album) {}
-	private void grow() {}
+	private void grow() {
+		Album[] grownCollection = new Album[albums.length + 4]
+			
+		for (int i = 0; i < albums.length; i++) {
+			grownCollection[i] = albums[i]
+		}
+		
+		//trying to call grown collcetion album instead
+		albums = new Album[grownCollection.length]
+			
+		for (int i = 0; i < grownCollection.length; i++) {
+			albums[i] = grownCollection[i]
+		}
+				
+	}
 	public boolean add(Album album) {}
 	public boolean lendingOut(Album album) {}
 	public boolean returnAlbum(Album album)

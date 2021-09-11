eac7b9c78fe3e631370527f63de83eaf01a243fb
emnelsonn21 <55959987+emnelsonn21@users.noreply.github.com>
2021-09-11 16:46:24 -0400

    Update CollectionManager.java

--- src/CollectionManager.java
+++ src/CollectionManager.java
@@ -4,11 +4,43 @@ public class CollectionManager {
   system.out.println("Collection Manager starts running");
   
   public void run() {
-    String userInput;
-    Scanner scanner = new Scanner(System.in);
+    String input;
+    Scanner sc = new Scanner(System.in);
+    StringTokenizer = new StringTokenizer(input, ",")
     
-    while (userInput = scanner.nextLine() != 'Q') {
+    
+    while (input = scanner.nextLine() != 'Q') {
       //read each line and see what to do - [A]dd, [D]elete, [L]end, [R]eturn, [P][PD][PG] display
+      
+      while (nextInput.hasMoreElements()) {
+        if (input.nextToken == 'A') {
+        }
+        
+        if (input.nextToken =='D') {
+        }
+        
+        if (input.nextToken == 'L') {
+        }
+        
+        if (input.nextToken == 'R') {
+        }
+        
+        if (input.nextToken == 'P') {
+        }
+        
+        if (input.nextToken == 'PD') {
+        }
+        
+        if (input.nextToken == 'PG') {
+        }
+        
+        else {
+          system.out.println("Invalid Command!")
+        }
+        
+      }
+      
+      
     }
   }
   

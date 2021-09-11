ec21c147864403d6e7c58a6c52667a01213b3451
emnelsonn21 <55959987+emnelsonn21@users.noreply.github.com>
2021-09-11 15:41:40 -0400

    Update CollectionManager.java

--- src/CollectionManager.java
+++ src/CollectionManager.java
@@ -1,4 +1,17 @@
+import java.util.Scanner;
 
 public class CollectionManager {
+  system.out.println("Collection Manager starts running");
+  
+  public void run() {
+    String userInput;
+    Scanner scanner = new Scanner(System.in);
+    
+    while (userInput = scanner.nextLine() != 'Q') {
+      //read each line and see what to do - [A]dd, [D]elete, [L]end, [R]eturn, [P][PD][PG] display
+    }
+  }
+  
+  system.out.println("Collection Manager terminated");
 
 }

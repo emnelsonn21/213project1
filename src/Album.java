2c7ea17347be3ab0006f4fbd18bfdba243c04f9f
emnelsonn21 <55959987+emnelsonn21@users.noreply.github.com>
2021-09-20 22:34:21 -0400

    Update Album.java

--- src/Album.java
+++ src/Album.java
@@ -1,4 +1,5 @@
 
+
 public class Album {
 	private String title;
 	  private String artist;
@@ -7,8 +8,53 @@ public class Album {
 	  private boolean isAvailable;
 	  
 	  @Override
-	  public boolean equals(Object obj) { }
+	  public boolean equals(Object obj) {
+		return false; 
+		}
 	  
 	  @Override
-	  public String toString() { }
+	  public String toString() { 
+		  return "yoyo";
+	  }
+	  
+	  
+	  public String getTitle() {
+		  return title;
+	  }
+	  
+	  public void setTitle(String newTitle) {
+		  this.title = newTitle;
+	  }
+	  
+	  public String getArtist() {
+		  return artist;
+	  }
+	  
+	  public void setArtist(String newArtist) {
+		  this.artist = newArtist;
+	  }
+	  
+	  public Genre getGenre() {
+		  return genre;
+	  }
+	  
+	  public void setGenre(Genre newGenre) {
+		  this.genre = newGenre;
+	  }
+	  
+	  public Date getReleaseDate() {
+		  return releaseDate;
+	  }
+	  
+	  public void setDate(Date newReleaseDate) {
+		  this.releaseDate = newReleaseDate;
+	  }
+	  
+	  public boolean getIsAvailable() {
+		  return isAvailable;
+	  }
+	  
+	  public void setIsAvailable(boolean newIsAvailable) {
+		  this.isAvailable = newIsAvailable;
+	  }
 }

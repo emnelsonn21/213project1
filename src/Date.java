411108b941b5fba862788d37945e5ba6716802e7
emnelsonn21 <55959987+emnelsonn21@users.noreply.github.com>
2021-09-21 19:57:55 -0400

    Update Date.java

--- src/Date.java
+++ src/Date.java
@@ -1,27 +1,34 @@
-import java.util.Calendar
+import java.util.Calendar;
+import java.util.StringTokenizer;
 
 public class Date implements Comparable<Date> {
 	private int year;
 	private int month;
 	private int day;
 	
-	//absolutely no idea if this is even remotely correct
-	public Date(String date) {
-		StringTokenizer tokens = new StringTokenizer(date, "/")
-		day = tokens.nextToken;
-		month = tokens.nextToken;
-		year = tokens.nextToken;
-		
-		Calendar calendar = Calendar.getInstance();
-		calendar.clear;
-		calendar.set(Calendar.YEAR, year);
-		calendar.set(Calendar.MONTH, month);
-		calendar.set(Calendar.DATE, day);
+	public Date(String date) {}
+	public Date() {}
 	
-	}
 	
-	public Date() {
-		Calendar calendar = Calendar.getInstance();
+	public Calendar getDate(String dat) {
+		StringTokenizer tokenizer = new StringTokenizer(dat, "/");
+		if (tokenizer.hasMoreElements()) {
+			try {
+				month = Integer.valueOf(tokenizer.nextToken());
+				day = Integer.valueOf(tokenizer.nextToken());
+				year = Integer.valueOf(tokenizer.nextToken());
+			}
+			catch (NumberFormatException nfe) {
+				throw new IllegalArgumentException("invalid date");
+		    }
+			
+			Calendar calendar = Calendar.getInstance();
+			calendar.set(Calendar.YEAR, year);
+			calendar.set(Calendar.MONTH, month);
+			calendar.set(Calendar.DATE, day);
+			return calendar;
+	    }
+		return null;
 	}
 	
 	public boolean isValid() {}

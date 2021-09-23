00cad5ed2540e5331052b6db7cfbf1a6057a6952
emnelsonn21 <55959987+emnelsonn21@users.noreply.github.com>
2021-09-22 21:34:49 -0400

    Update Date.java

--- src/Date.java
+++ src/Date.java
@@ -6,12 +6,10 @@ public class Date implements Comparable<Date> {
 	private int month;
 	private int day;
 	
-	public Date(String date) {}
-	public Date() {}
-	
-	
-	public Calendar getDate(String dat) {
-		StringTokenizer tokenizer = new StringTokenizer(dat, "/");
+	//this is a constructor. it doesn't return a value. it makes a Date object when 
+	//you give it a string that's valid
+	public Date(String date) {
+		StringTokenizer tokenizer = new StringTokenizer(date, "/");
 		if (tokenizer.hasMoreElements()) {
 			try {
 				month = Integer.valueOf(tokenizer.nextToken());
@@ -26,13 +24,113 @@ public class Date implements Comparable<Date> {
 			calendar.set(Calendar.YEAR, year);
 			calendar.set(Calendar.MONTH, month);
 			calendar.set(Calendar.DATE, day);
-			return calendar;
 	    }
-		return null;
+	}
+	public Date() {
+		Calendar today = Calendar.getInstance();
 	}
 	
-	public boolean isValid() {}
+	
+	public static final int QUADRENNIAL = 4;
+	public static final int CENTENNIAL = 100;
+	public static final int QUARTERCENTENNIAL = 400;
+	public static final int THE_EIGHTYS = 1980;
+	public static final int THIS_YEAR = 2021;
+	public static final int JANUARY = 1;
+	public static final int DECEMBER = 12;
+	public static final int FIRST_DAY = 1;
+	public static final int LAST_DAY = 31;
+	public static final int THIRTIETH_DAY = 30;
+	public static final int FEB_LAST_DAY = 28;
+	public static final int FEB_LEAP_LAST_DAY = 29;
+	public static final int FEB = 2;
+	public static final int APR = 4;
+	public static final int JUNE = 6;
+	public static final int SEPT = 9;
+	public static final int NOV = 11;
+	public boolean isValid() {
+		
+		
+		if (day > LAST_DAY) {
+			return false;
+		}
+		
+		if (month < JANUARY || month > DECEMBER) {
+			return false;
+		}
+		
+		if (year < THE_EIGHTYS || year > THIS_YEAR) {
+			return false;
+		}
+		
+		
+		
+		if (month == APR || month == JUNE || month == SEPT || month == NOV) {
+			if (day > 30) {
+				return false;
+			}
+		}
+		
+		
+		if (month == FEB) {
+			if (day > FEB_LAST_DAY) {
+				return false;
+			}
+			
+			if (day == FEB_LAST_DAY && year%QUADRENNIAL != 0) {
+				if (year % QUADRENNIAL == 0) {
+					if (year % CENTENNIAL == 0) {
+						if (year % QUARTERCENTENNIAL == 0) {
+							return true;
+						}
+					}
+					return false;
+				}
+				return false;
+			}
+			
+		}
+		
+		return true;
+	}
 	
 	@Override
-	public int compareTo(Date date) {}
+	public int compareTo(Date date) {
+		int yeardiff = this.year - date.year;
+		int monthdiff = this.month - date.month;
+		int daydiff = this.day - date.day;
+		
+		//return positive if "date" is older
+		//return negative if "this" is older
+		//example:    9/22/2021 compareTo ( 1/1/2021)
+		//"date" is older, yeardiff is pos, return 1 (positive)
+		
+		if (yeardiff > 0) {
+			return 1;
+		}
+		
+		if (yeardiff < 0) {
+			return -1;
+		}
+		
+		if (monthdiff > 0) {
+			return 1;
+		}
+		
+		if (monthdiff < 0) {
+			return -1;
+		}
+		
+		if (daydiff > 0) {
+			return 1;
+		}
+		
+		if (daydiff < 0) {
+			return -1;
+		}
+		
+		return 0;
+	}
+	
+	
 }

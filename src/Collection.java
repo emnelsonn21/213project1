bcc7f7cf4fd0fc6d92948774d25505af782b7c59
cris431 <32299513+cris431@users.noreply.github.com>
2021-09-25 18:54:51 -0400

    Update Collection.java

--- src/Collection.java
+++ src/Collection.java
@@ -4,6 +4,13 @@ import java.util.Arrays;
 public class Collection {
 	private Album[] albums; //how to initialize it to size 4 if we can't change instance variables?
 	private int numAlbums;
+	
+	/**
+	Finds the index where the album is located in the albums array
+	@param album the album being looked for
+	@return the index of the album if found, -1 otherwise
+	@author Cristofer Gomez-Martinez
+	*/
 	private int find(Album album) {
 	for (int i = 0; i< numAlbums; i++) {
 			if (album.equals(albums[i])) {
@@ -15,14 +22,29 @@ public class Collection {
 		return -1;
 	}
 	
+	/**
+	Returns the array of albums
+        @return albums array
+	@author Emily Nelson 
+	*/
 	public Album[] getAlbums() {
 		  return albums;
 	  }
 	  
+	  /**
+	  Sets the albusm array to a new albums array
+	  @param newAlbum the new albums array to set
+	  @author Emily Nelson
+	  */
 	  public void setAlbums(Album[] newAlbum) {
 		  this.albums = newAlbum;
 	  }
-	  
+	
+	/**
+	Increases the capacity of the albums array by 4
+	Grown whenever array is full
+	@author Emily Nelson
+	*/
 	private void grow() {
 		Album[] grownCollection = new Album[albums.length + 4];
 			
@@ -44,6 +66,15 @@ public class Collection {
 		//}
 				
 	}
+	
+	/**
+	Checks if an album can be added to the albums array
+	Adds album to albums array if album is not found in array
+	Does nothing if the album is alrady in the albums array
+	@param album the album that is to be added
+	@return true if album is not in albums array, false otherwise
+	@author Cristofer Gomez-Martinez, Emily Nelson
+	*/
 	public boolean add(Album album) {
 		//use find to check if already in collection
 		//put attributes of album in the array
@@ -64,6 +95,14 @@ public class Collection {
 		return true;
 	}
 	
+	/**
+	Checks if an album can be removed from the albums array
+	Removes album from albums array if album is found
+	Does nothing if the album is not in albums array
+	@param album the album that is to be removed
+	@return true if album is in albums array, false otherwise
+	@author Emily Nelson
+	*/
 	public boolean remove(Album album) {
 		if (find(album) == -1) {
 			return false;
@@ -78,6 +117,14 @@ public class Collection {
 		return true;
 	}
 	
+	/**
+	Checks if the album can be lended out
+	Lends out the album if the album is found in the albums array
+	Does nothing if the albums is not in the albums arrays
+	@param album the album that is to be lended out
+	@return true if album is in albums array, false otherwise
+	@author Emily Nelson
+	*/
 	public boolean lendingOut(Album album) {
 		if (find(album) == -1) {
 			return false;
@@ -86,6 +133,15 @@ public class Collection {
 		albums[find(album)].setIsAvailable(false);
 		return true;
 	}
+	
+	/**
+	Checks if the album can be returned
+	Returns the album if the album is found in the albums array
+	Does nothing if the albums is not in the albums arrays
+	@param album the album that is to be returned
+	@return true if album is in albums array, false otherwise
+	@author Emily Nelson
+	*/
 	public boolean returnAlbum(Album album) {
 		if (find(album) == -1) {
 			return false;
@@ -95,6 +151,10 @@ public class Collection {
 		return true;
 	}
 	
+	/**
+	Displays the list of albums in the albums array without specifying the order
+	@author Cristofer Gomez-Martinez
+	*/
 	public void print() {
 		String placeHolder = "";
 		for(int i = 0; i < albums.length; i++) {
@@ -103,6 +163,10 @@ public class Collection {
 		}
 	}
 	
+	/**
+	Displays the list of albums in the albums array by release date
+	@author Cristofer Gomez-Martinez
+	*/
 	public void printByReleaseDate() {
 		Album[] sortedCollection = new Album[albums.length];
 		//sortedCollection will be a copy of the albums array that we will modify so to not change the original

e2429c340e63d66f1d82f6a6e2bc2f46a29c35f2
emnelsonn21 <55959987+emnelsonn21@users.noreply.github.com>
2021-09-27 21:27:57 -0400

    Update Genre.java

--- src/Genre.java
+++ src/Genre.java
@@ -1,4 +1,8 @@
-
+/**
+ This class defines the object Genre and lists all possible genres
+ @author Emily Nelson, Cristofer Gomez-Martinez
+ */
 public enum Genre {
 	Classical, Country, Jazz, Pop, Unknown
+	
 }
